1a253abc5493292013374c2b1738f6023cb18a5e
Harrybeaty <1beatyh@gmail.com>
2024-07-27 11:48:44 +0100

    Completed Hangman Game

--- Hangman.py
+++ Hangman.py
@@ -1,22 +1,46 @@
 import random
 
-word_list = ["ardvark", "carrot", "tiger", "chicken", "dog"]
+from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
 word_length = len(chosen_word)
 blank = []
 for i in range(word_length):
     blank.append("_")
 
-user_letter = input("Please choose a letter: ").lower()
+end_of_game = False
+lives = 6
+from hangman_art import logo, stages
+print(logo)
+guessed_letters = []  
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == user_letter:
-        print("right")
-        blank[position] = letter
-    else:
-        print("wrong")
+while not end_of_game:
+    user_letter = input("Please choose a letter: ").lower()  
 
-print(blank)
+# Update blank if letter in word.
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == user_letter:
+            blank[position] = letter
+
+# Lose a life when letter not in word.
+    if user_letter not in blank:
+        print(f"You guessed '{user_letter}' which is not in the word, you lose a life.")
+        lives -= 1
+
+    guessed_letters.append(user_letter)   
+    print(stages[lives])
+    print(f"Previously guessed letters: {guessed_letters}")                                            # Print Ascii Art.
+# Lose when user has no lives.
+    if lives == 0:
+        end_of_game = True
+        print("You Lose!")
+        print(f"The word was {chosen_word}")
+
+    print(blank)
+
+# Win game when blank contains the word.
+    if "_" not in blank:
+        end_of_game = True
+        print("You Win!")
+
