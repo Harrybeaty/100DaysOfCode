edb7e6baf41bf16a01b0f12c86353e528ee2568c
Harrybeaty <1beatyh@gmail.com>
2024-08-14 20:08:06 +0100

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -30,19 +30,26 @@ while run:
             another_card = input("Do you want to Stick or Twist? ").lower()
             if another_card == "stick":
                 twist = False
+                # Draw a card for dealer while total is less than 17
                 while sum(cards_dict["dealer"]) < 17:
-                    # needs to display card when it is drawn
-                    cards_dict["dealer"].append(random.choice(cards))
-                    # need to check id dealer is bust.
+                    dealer_new_card = random.choice(cards)
+                    print(f"Drawn card: {dealer_new_card}")
+                    cards_dict["dealer"].append(user_new_card)
+                    # need to check if dealer is bust.
+                    dealer_total = sum(cards_dict["dealer"])
+                    if dealer_total > 21:
+                        print("Dealer is bust, you win.")
                     # if dealer bust check they if they have an ace.
                 calc_totals()
             else:
-                # Needs to display card when it is drawn
-                cards_dict["user"].append(random.choice(cards))
+                # If user twists, draw a card, sum all cards, display all cards
+                user_new_card = random.choice(cards)
+                print(f"Drawn card: {user_new_card}")
+                cards_dict["user"].append(user_new_card)
                 user_total = sum(cards_dict["user"])
-                # Needs to display cards
+                print(f"Your Cards: {cards_dict["user"]}")
+                # Check if User is Bust
                 if user_total > 21:
                     twist = False
                     print("You are Bust!")
-                # Need to check if user has ace and change it to 1
-                
+                    # Check if user has an Ace and change the value to 1           
