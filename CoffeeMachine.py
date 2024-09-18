060b34af9b9476a3f29e3225bfab88696d3b9ee5
Harrybeaty <1beatyh@gmail.com>
2024-09-18 06:38:02 +0100

    Finished my version of the coffee machine

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -30,30 +31,43 @@ resources = {
     "coffee": 100,
 }
 
-def check_resources(ingredients, coffees, user_choice):
+def check_resource(ingredients, coffee):
 
     for key in ingredients:
-        if ingredients[key] >= coffees[user_choice]["ingredients"][key]:
+        if ingredients[key] < coffee[key]:
             print(f"Insufficient {key}")
-# Stuck here i need to return false
+            return False
 
+def process_coins(q, d ,n, p):
+    total = 0.25 * q + 0.1 * d + 0.05 * n + 0.01 * p
+    return total
 
 # Get user's input
-user_input = input("What would you like? (espresso/latte/cappuccino): ")
+on = True
+while on:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ")
 
-user_choice_water = MENU[user_input]["ingredients"]["water"]
-user_choice_milk = MENU[user_input]["ingredients"]["milk"]
-user_choice_coffee = MENU[user_input]["ingredients"]["coffee"]
-
-if user_input == "off":
-    exit()
-elif user_input == "report":
-    print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g ")
-elif user_input == "espresso" or user_input == "latte" or user_input == "cappuccino":
-    # Check if there are enough resources to make coffee.
-    if resources["water"] >= user_choice_water and resources["milk"] >= user_choice_milk and resources["coffee"] >= user_choice_coffee:
-        # Minus ingredients for a coffee from resources.
-        for key in resources:
-            resources[key] = resources[key] - MENU[user_input]["ingredients"][key]
-    else:
-        print("insufficient")
+    if user_input == "off":
+        on = False
+    elif user_input == "report":
+        print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g ")
+    elif user_input == "espresso" or user_input == "latte" or user_input == "cappuccino":
+        coffee_resource = MENU[user_input]["ingredients"]
+        coffee_cost = MENU[user_input]["cost"]
+        # Check if there are enough resources to make coffee.
+        if check_resource(resources, coffee_resource) != False:
+            # Minus ingredients for a coffee from resources.
+            print("Please insert coins.")
+            quarters = int(input("How many quarters? "))
+            dimes = int(input("How many dimes? "))
+            nickles = int(input("How many nickles? "))
+            pennies = int(input("How many pennies? "))
+            total = process_coins(quarters, dimes, nickles, pennies)
+            if total >= coffee_cost:
+                for key in resources:
+                    resources[key] = resources[key] - coffee_resource[key]
+                revenue =+ coffee_cost
+                change = total - coffee_cost
+                print(f"You're change is ${change}.")
+            else:
+                print("Insufficient Money")
